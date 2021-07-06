8e75648f9cb87ac22428b50ed109a17086487f0e
Joon Park <aisgandy@hotmail.com>
2021-07-06 18:13:30 +0900

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by 박준우 on 2021/07/06.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
